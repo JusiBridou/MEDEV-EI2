704330d8c0147c7dd83a8cfeba7b46845c7f1bf9
yaelv <yaelv@130.66.205.62>
2025-11-10 17:17:30 +0100

    Changement message sortie

--- Monopoly/src/main/java/org/centrale/medev/monopoly/monopoly/Plateau.java
+++ Monopoly/src/main/java/org/centrale/medev/monopoly/monopoly/Plateau.java
@@ -3,8 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package org.centrale.medev.monopoly.monopoly;
+import static java.lang.Math.sqrt;
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 
 
@@ -83,5 +85,49 @@ public class Plateau {
         this.joueurs = joueurs;
     }
     
+    public void initPlateau(){
+        LinkedList<String> noms = new LinkedList<>();
+        noms.addAll(Arrays.asList("Case Départ","Boulevard de Belleville","Caisse de Communauté","Rue Lecourbe","Impôts sur le revenu",
+                "Gare Montparnasse","Rue de Vaugirard","Chance","Rue de Courcelles","Avenue de la République","Prison",
+                "Boulevard de la Villette","Compagnie de distribution d'électricité","Avenue de Neuilly","Rue de Paradis",
+                "Gare de Lyon","Avenue Mozart","Caisse de Communauté","Boulevard Saint-Michel","Place Pigalle","Parc Gratuit",
+                "Avenue Matignon","Chance","Boulevard Malesherbes","Avenue Henri-Martin","Gare du Nord","Faubourg Saint-Honoré",
+                "Place de la Bourse","Compagnie de distribution des eaux","Rue la Fayette","Allez en Prison","Avenue de Breteuil",
+                "Avenue Foch","Caisse de communauté","Boulevard des Capucines","Gare Saint-Lazare","Chance",
+                "Avenue des Champs-Elysées","Taxe de Luxe","Rue de la Paix"));
+        for(int i=0;i<40;i++){
+            
+            if(noms.get(i)=="Chance"||noms.get(i)=="Caisse de Communauté"){
+                plateau.add(new CaseCarte(noms.get(i),i,noms.get(i)));
+            }
+            else if(noms.get(i).substring(0,4)=="Gare"){
+                plateau.add(2000, new Gare(2000,null,i, noms.get(i)));
+            }
+            else if(noms.get(i)=="Allez en Prison"){
+                plateau.add(new GoPrison(noms.get(i),i));
+            }
+            else if(noms.get(i).substring(0,9)=="Compagnie"){
+                plateau.add(new Fournisseur( (int) (sqrt(i+1)),null,i, noms.get(i)));
+            }
+            else if(noms.get(i)=="Case Départ"||noms.get(i)=="Prison"||noms.get(i)=="Parc Gratuit"){
+                plateau.add(new Attente(noms.get(i),i));
+            }   
+            else if(noms.get(i)=="Taxe de Luxe"){
+                plateau.add(new Taxe(noms.get(i),i,200));
+            }  
+            else if(noms.get(i)=="Impôts sur le revenu"){
+                plateau.add(new Taxe(noms.get(i),i,100));
+            }  
+            else{
+                plateau.add(new Constructible(0, (int) (sqrt(i+1)*1000),null,i,noms.get(i)));
+            }
+        }
+        
+        for(int i=0; i < 25; i++){
+            piocheChance.add(new Carte("chance"));
+            piocheCommunaute.add(new Carte("communaute"));
+        }
+        
+    }
 }
 
