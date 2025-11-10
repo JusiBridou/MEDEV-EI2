53d18c00a13d29a6a6481c99fee5823250aa2c8a
alexElArte <alexandre>
2025-11-10 15:27:21 +0100

    Correction de la case GoPrison

--- Monopoly/src/main/java/org/centrale/medev/monopoly/monopoly/GoPrison.java
+++ Monopoly/src/main/java/org/centrale/medev/monopoly/monopoly/GoPrison.java
@@ -28,6 +28,7 @@ public class GoPrison extends CaseSpeciale {
 		for (Case case : plateau) {
 			if (case.getNom().equals("Prison")) {
 				j.setCase(case);
+				j.setTourRestantPrison(3);
 				break;
 			}
 		}
