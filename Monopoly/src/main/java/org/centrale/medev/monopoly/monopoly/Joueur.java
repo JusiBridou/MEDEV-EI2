35e1b3644e0f7f3365145e315506e63e4da8fcf3
Max <Max@localhost>
2025-11-10 14:57:48 +0100

    Merge origin/main into main
    # Conflicts:
    #	Monopoly/src/main/java/org/centrale/medev/monopoly/monopoly/Joueur.java

--- Monopoly/src/main/java/org/centrale/medev/monopoly/monopoly/Joueur.java
+++ Monopoly/src/main/java/org/centrale/medev/monopoly/monopoly/Joueur.java
@@ -7,7 +7,7 @@ package org.centrale.medev.monopoly.monopoly;
 import java.util.List;
 
 /**
- *
+ *  Un joueur
  * @author Max
  */
 public class Joueur {
@@ -18,46 +18,105 @@ public class Joueur {
     private Plateau plateau;
     
     private List<Case> caseDetenue;
+    
+    /**
+     *  Constructeur d'un joueur
+     * @param nom
+     * @param fortune
+     * @param position
+     * @param plateau
+     * @param caseDetenue
+     */
+    public Joueur(String nom, int fortune, int position, Plateau plateau, List<Case> caseDetenue) {
+        this.nom = nom;
+        this.fortune = fortune;
+        this.position = position;
+        this.plateau = plateau;
+        this.caseDetenue = caseDetenue;
+        
+    }
 
+    /**
+     *
+     * @return
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     *
+     * @param nom
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getFortune() {
         return fortune;
     }
 
+    /**
+     *
+     * @param fortune
+     */
     public void setFortune(int fortune) {
         this.fortune = fortune;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getPosition() {
         return position;
     }
 
+    /**
+     *
+     * @param position
+     */
     public void setPosition(int position) {
         this.position = position;
     }
 
+    /**
+     *
+     * @return
+     */
     public Plateau getPlateau() {
         return plateau;
     }
 
+    /**
+     *
+     * @param plateau
+     */
     public void setPlateau(Plateau plateau) {
         this.plateau = plateau;
     }
 
+    /**
+     *
+     * @return
+     */
     public List<Case> getCaseDetenue() {
         return caseDetenue;
     }
 
+    /**
+     *
+     * @param caseDetenue
+     */
     public void setCaseDetenue(List<Case> caseDetenue) {
         this.caseDetenue = caseDetenue;
     }
+
+    
     
     
 
