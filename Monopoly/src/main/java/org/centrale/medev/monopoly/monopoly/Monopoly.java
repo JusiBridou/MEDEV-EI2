ed421726cef886c6e21a2d1f96cdfefec66b732b
alex_el.arte <77207986+alexElArte@users.noreply.github.com>
2025-11-10 14:15:26 +0100

    Add files via upload

---
+++ Monopoly/src/main/java/org/centrale/medev/monopoly/monopoly/Monopoly.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.centrale.medev.monopoly.monopoly;
+
+/**
+ *
+ * @author Catherine
+ */
+public class Monopoly {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
