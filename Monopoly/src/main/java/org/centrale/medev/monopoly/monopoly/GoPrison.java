594095262871682dabae4f069e5d6dae8dc521cf
alexElArte <alexandre>
2025-11-10 15:23:55 +0100

    Mise en place des "cases prison" et "aller en prison"

--- Monopoly/src/main/java/org/centrale/medev/monopoly/monopoly/GoPrison.java
+++ Monopoly/src/main/java/org/centrale/medev/monopoly/monopoly/GoPrison.java
@@ -4,10 +4,32 @@
  */
 package org.centrale.medev.monopoly.monopoly;
 
+import java.util.ArrayList;
+
 /**
- *
+ * Classe de la case Aller en prison
  * @author Max
  */
-public class GoPrison {
-    
+public class GoPrison extends CaseSpeciale {
+	
+	/**
+	 * Constructeur de la case Aller en prison
+	 */
+    public GoPrison() {
+		super(30, "Aller en prison");
+	}
+	
+	/**
+	 * Fonction qui envoie le joueur en prison
+	 * @param j Le joueur à envoyer en prison
+	 */
+	public void envoyerJoueurPrison(Joueur j) {
+		ArrayList<Case> plateau = j.getPlateau().getPlateau();
+		for (Case case : plateau) {
+			if (case.getNom().equals("Prison")) {
+				j.setCase(case);
+				break;
+			}
+		}
+	}
 }
